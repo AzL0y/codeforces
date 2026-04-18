ebc621ba9fef3ebe400e2545b418ed286cee6fba
AzL0y <rayan.bensouiah@gmail.com>
2026-04-18 20:15:15 +0200

    Create watermelon.c
    Accepted

---
+++ watermelon.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+/*
+* Says whether weight can be split into 2 even numbers
+
+IN : weight (int w)
+OUT : YES / NO
+*/
+
+int main()
+{
+    int w;
+    scanf("%d", &w);
+
+    if (w % 2 == 0 && w > 2) printf("YES");
+    else printf("NO");
+    return 0;
+}
