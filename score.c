c74d4dba3c04f373222a1a342feb73e1168115fe
AzL0y <rayan.bensouiah@gmail.com>
2026-04-20 15:59:09 +0200

    Create score.c

---
+++ score.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+* Problem 158A
+* n contestants, at least k places in next round, score > k-th element to advance
+
+IN :
+    int n : number of contestants, int k : index of the score to check
+    n integers representing the score of each contestant, (non increasing sequence)
+OUT : 
+    int x : number of contestants who advance
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, k, x = 0;
+    scanf("%d %d", &n, &k);
+
+    int score, kth = 0;
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &score);
+
+        if (i == k)
+            kth = score;
+
+        if (score > 0 && score >= kth)
+            x++;
+    }
+
+    printf("%d\n", x);
+    return 0;
+}
