14a17199e0f2250555948cfa58a779b750da1f39
AzL0y <rayan.bensouiah@gmail.com>
2026-04-20 15:29:17 +0200

    Create bitpp.c

---
+++ bitpp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+* Calcul
+
+IN :
+    int n : number of lines
+    string of 3 chars : ++X, --X, X++ , X--
+OUT : 
+    int x : final value of x
+*/
+
+int main()
+{
+    int n, x = 0;
+    char s[5];
+
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        scanf("%s", s);
+        if ((s[1] == '+')) x++;
+        else x--;
+    }
+    printf("%d\n", x);
+    return 0;
+}
